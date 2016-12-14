f1b2bce50f662bc1e0995b820834786e8b143fee
makarun <jaroslawmaka@gmail.com>
2016-12-14 18:32:13 +0100

    about page

--- rango/views.py
+++ rango/views.py
@@ -7,4 +7,6 @@ def index(request):
     #return HttpResponse("Rango says hello! <br/> <a href='/rango/about'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<br/> <a href='/rango/'> MainPage </a>")
+    context_dict = {'boldmessege': 'This tutorial has been put together by devil'}
+    return render(request, 'rango/about.html', context=context_dict)
+    #return HttpResponse("Rango says here is the about page.<br/> <a href='/rango/'> MainPage </a>")
